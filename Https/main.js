fd07e863443614c609447aa0e6b92b65773634f4
EA03cs <elsayedatef469@gmail.com>
2026-04-12 20:15:18 +0200

    finally >> made find user endpoint by myself

--- Https/main.js
+++ Https/main.js
@@ -9,21 +9,50 @@ function requestListener() {
     }
     );
 }
+
 const server = http.createServer((req, res) => {
     const { url, method } = req;
     console.log({ url, method });
-    if (req.url === '/profile' && req.method === 'GET') {
-        console.log('Profile page');
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('Welcome to your profile!');
-        res.end();
+    if (req.url === '/findUser' && req.method === 'POST') {
+        let body = '';
+
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            body = JSON.parse(body);
+
+            const { name } = body;
+
+            const findUsers = JSON.parse(
+                fs.readFileSync(path.resolve('./Https/users.json'), 'utf-8')
+            );
+
+            const users = findUsers
+                .filter((u) => u.name.toLowerCase().includes(name.toLowerCase()))
+                .map((u) => ({
+                    name: u.name,
+                    email: u.email,
+                    id: u.id
+                }));
+            if (users) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(users));
+            } else {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: 'User not found' }));
+            }
+        });
     } else if (req.url === '/about' && req.method === 'GET') {
         console.log('About page');
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('About page');
     } else if (req.url === '/users' && req.method === 'GET') {
+        const users = JSON.parse(
+            fs.readFileSync(path.resolve('./Https/users.json'), 'utf-8'));
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify(user));
+        res.write(JSON.stringify(users));
         res.end();
     } else if (req.url === '/signup' && req.method === 'POST') {
         let body = '';
