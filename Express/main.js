cdfe2ef66288f6ba71e7315b982a0b939deeb9e5
EA03cs <elsayedatef469@gmail.com>
2026-04-17 19:53:25 +0200

    signup and login

--- Express/main.js
+++ Express/main.js
@@ -2,24 +2,50 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let isLoggedIn = false;
+// let isLoggedIn = false;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-const authMiddleware = (req, res, next) => {
-    if (isLoggedIn) {
-        next();
+// const authMiddleware = (req, res, next) => {
+//     if (isLoggedIn) {
+//         next();
+//     } else {
+//         res.status(401).send('Unauthorized');
+//     }
+// };
+// app.use(authMiddleware);
+
+// app.get('/dashboard', (req, res, next) => {
+//     res.send('Welcome to the dashboard!');
+// });
+
+// app.get('/login', (req, res, next) => {
+//     res.send('Login successful!');
+// });
+let users = [];
+app.use(express.json());
+app.post('/signup', (req, res, next) => {
+    console.log(req.body);
+    const [username, email, password] = req.body;
+    const user = users.find(u => u.email === email);
+    if (user) {
+        res.status(409).send('User already exists');
     } else {
-        res.status(401).send('Unauthorized');
+        users.push({ username, email, password, id: Date.now() });
+        res.send('Signup successful!',users);
     }
-};
-app.use(authMiddleware);
 
-app.get('/dashboard', (req, res, next) => {
-    res.send('Welcome to the dashboard!');
-});
 
-app.get('/login', (req, res, next) => {
-    res.send('Login successful!');
-});
+},
+    app.post('/login', (req, res, next) => {
+        console.log(req.body);
+        const { username, password } = req.body;
+        const user = users.find(u => u.username === username && u.password === password);
+        if (user) {
+            res.send('Login successful!', user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    })
+);
