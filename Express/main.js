4396edee6da012e6e2d811589624ceafb3a84e92
EA03cs <elsayedatef469@gmail.com>
2026-04-17 18:50:33 +0200

    middleware concept

--- Express/main.js
+++ Express/main.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+let isLoggedIn = false;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
+const authMiddleware = (req, res, next) => {
+    if (isLoggedIn) {
+        next();
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+};
+app.use(authMiddleware);
+
+app.get('/dashboard', (req, res, next) => {
+    res.send('Welcome to the dashboard!');
+});
+
+app.get('/login', (req, res, next) => {
+    res.send('Login successful!');
 });
