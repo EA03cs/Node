39a04d4a56eb47a3350a2ce989529b87e19c32e8
Elsayed <Elsayedatef469@gmail.com>
2026-05-11 16:19:51 +0300

    feat: update user service to implement token-based user retrieval and modify user controller routes

--- SarahaApp/src/modules/user/user.controller.js
+++ SarahaApp/src/modules/user/user.controller.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, getUserById, searchUser, updateUser, deleteUser } from "./user.service.js";
+import { getAllUsers, getUserBytoken, searchUser, updateUser, deleteUser } from "./user.service.js";
 
 const router = Router();
 router.get("/search", searchUser);
 router.get('/', getAllUsers);
 router.get('/getUsers', getAllUsers);
-router.get('/:id/profile', getUserById);
+router.get('/profile', getUserBytoken);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
 export default router;
--- SarahaApp/src/modules/user/user.service.js
+++ SarahaApp/src/modules/user/user.service.js
@@ -1,10 +1,34 @@
-
-export const getAllUsers = ( (req, res) => {
+import UserModel from "../../db/model/UserModel.js";
+import { asyncHandler } from "../../utils/response.js";
+import * as dbService from "../../db/service/db.service.js";
+import {successResponse,globalErrorHandler} from "../../utils/response.js";
+import bcrypt from "bcryptjs";
+import * as hashSecurity from "../../utils/security/hash.security.js";
+import jwt from "jsonwebtoken";
+export const getAllUsers = (async (req, res , next) => {
 res.status(200).json({ message: "success", data: [] });
 });
 
+export const getUserBytoken = (async (req, res , next) => {
+    console.log(req.headers);
+    const {authorization} = req.headers;
+    console.log(authorization);
+    const token = authorization.replace("Bearer ", "");
+    const decodedToken = jwt.verify(token, "your_secret_key_here");
+    console.log(decodedToken)
+    const user = await dbService.findone({
+        model: UserModel, filter: { _id: decodedToken.userId }
+    });
+    if(!user) {
+        return globalErrorHandler(res, 404, false, 'User not found', null);
+    }
+    return successResponse(res, 200, true, 'User found successfully', user);
+});
+
 export const getUserById = ( (req, res) => {
+
 res.status(200).json({ message: "success", data: null });
+
 });
 
 export const searchUser = ( (req, res) => {
